0982c4bcf3716f553564d422a4a609280429d20b
Elias Pinheiro <pinheirinho2004@gmail.com>
2023-12-06 21:55:40 -0300

    drawer

--- app/src/main/java/com/example/eventqrcode/MainActivity.java
+++ app/src/main/java/com/example/eventqrcode/MainActivity.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import com.example.eventqrcode.controller.EventController;
@@ -77,6 +83,30 @@ public class MainActivity extends AppCompatActivity {
                 String item = String.valueOf(adapterView.getAdapter().getItem(i));
                 idEvento = Integer.parseInt(item.split(" ")[0]);
                 abrirActivityDeEvento();
+            }
+        });
+    }
+
+    public void showPopupWindow(View view){
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.drawer, null);
+
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.NO_GRAVITY, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
             }
         });
     }
